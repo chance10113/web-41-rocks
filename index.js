3f6eb797cf53a37ba319c7887c0e1182bfce4d9d
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-05-06 12:03:05 -0500

    api change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 
 server.use(express.json());
 
-server.get("api", (res, req) => {
+server.get("/api", (res, req) => {
   res.json({
     cohort: 'Web 41",',
   });
