a879077b07de0a547cdafd19a0845620d1dd5f3b
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-05-06 11:48:55 -0500

    Setting up

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("ITS WORKING");
+
+const express = require("express");
+
+const server = express();
+
+const PORT = process.env.PORT || 5000
+
+server.use(express.json());
+
+server.get("api", (res, req) => {
+  res.json({
+    cohort: 'Web 41",',
+  });
+});
+
+
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+});
