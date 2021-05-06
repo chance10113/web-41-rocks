cd5750f86fbebed2bdc2413a488006170b833d9a
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-05-06 12:16:37 -0500

    api change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const server = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 server.use(express.json());
 
@@ -12,10 +12,9 @@ server.get("/api/web", (res, req) => {
   res.json({
     cohort: 'Web 41",',
   });
+  console.log("req", req);
 });
 
-
-
 server.listen(PORT, () => {
-    console.log(`listening on ${PORT}`);
+  console.log(`listening on ${PORT}`);
 });
